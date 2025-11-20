70ce6b340d49c42b8be079e34c6e630689cb5bce
ringowomuite <huiqiteng05@gmail.com>
2025-11-20 22:48:35 +0900

    add cardInfo
    カード情報の追加

---
+++ js/card.js
@@ -0,0 +1,7 @@
+export const cards = [
+  { id: 1, name: "マトリョーシカ", base: 1, type: "turn_plus" },
+  { id: 2, name: "ふたご", base: 2, type: "turn_2_only" },
+  { id: 3, name: "オーブン", base: 3, type: "turn_3_after" },
+  { id: 4, name: "ライガー", base: 4, type: "turn_before_4" },
+  { id: 5, name: "バニラ", base: 5, type: "none" },
+];

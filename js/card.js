c51fc01c24fd9fd8c9beb5ec7b3c1abbb090838e
ringowomuite <huiqiteng05@gmail.com>
2025-11-20 23:33:09 +0900

    edit cardInfo
    カード情報を変更

--- js/card.js
+++ js/card.js
@@ -1,7 +1,7 @@
 export const cards = [
-  { id: 1, name: "マトリョーシカ", base: 1, type: "turn_plus" },
-  { id: 2, name: "ふたご", base: 2, type: "turn_2_only" },
-  { id: 3, name: "オーブン", base: 3, type: "turn_3_after" },
-  { id: 4, name: "ライガー", base: 4, type: "turn_before_4" },
-  { id: 5, name: "バニラ", base: 5, type: "none" },
+  { id: 1, name: "シングル", base: 1, type: "none" },
+  { id: 2, name: "ダブル", base: 2, type: "none" },
+  { id: 3, name: "トリプル", base: 3, type: "none" },
+  { id: 4, name: "クアドラプル", base: 4, type: "none" },
+  { id: 5, name: "クインタプル", base: 5, type: "none" },
 ];

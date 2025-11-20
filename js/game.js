cbfddc9a96db5ce173606246df28d7cbf2f4e8c8
ringowomuite <huiqiteng05@gmail.com>
2025-11-20 23:34:07 +0900

    make stab.js
    stab.jsを作成。
    スタブを呼び出し、手札にカード5枚を格納する。

--- js/game.js
+++ js/game.js
@@ -1,5 +1,14 @@
 import * as Config from "./config.js";
+import * as Stab from "./stab.js";
+
+let enemyHand = [];
+let mineHand = [];
 
 export function start() {
+
+    // TODO カード選択フェーズ
+    // スタブ処理として、拡張var1のカード5枚を選択とする
+    enemyHand.push(Stab.choiceCards());
+    mineHand.push(Stab.choiceCards());
 
 }
---
+++ js/stab.js
@@ -0,0 +1,5 @@
+import * as Card from "./card.js";
+
+export function choiceCards() {
+    return Card.cards;
+}

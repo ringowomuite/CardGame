aff2e208d46d39b14393f853bcf603a5f787e4ea
ringowomuite <huiqiteng05@gmail.com>
2025-11-23 15:23:02 +0900

    Update game.js
    二人称をそれぞれの名前の定数で保持(ui.js)
    得点計算を追加
    ターン計算を追加
    main.jsのクリックイベントをprocessDicision()として移動

--- js/game.js
+++ js/game.js
@@ -40,7 +40,7 @@ export async function cardJudge(mineCard, enemyCard, mineIndex, enemyIndex) {
     UI.renderHand(mineHand, CONFIG.MINE);
     enemyHand[enemyIndex] = null;
     UI.renderHand(enemyHand, CONFIG.ENEMY);
-    
+
     // ★ 0.5秒待つ
     await new Promise(resolve => setTimeout(resolve, 500));
 
@@ -48,36 +48,80 @@ export async function cardJudge(mineCard, enemyCard, mineIndex, enemyIndex) {
         // 得点の変更、表示、ログ出力
         minePoint++;
         UI.updatePoint(minePoint, CONFIG.MINE);
-        alert(CONFIG.CARD_JUDGE_WIN("あなた"));
-        UI.addLog(CONFIG.CARD_JUDGE_WIN("あなた"));
+        alert(CONFIG.CARD_JUDGE_WIN(UI.MINE_NAME));
+        UI.addLog(CONFIG.CARD_JUDGE_WIN(UI.MINE_NAME));
     } else if (minePower < enemyPower) {
         // 得点の変更、表示、ログ出力
         enemyPoint++;
         UI.updatePoint(enemyPoint, CONFIG.ENEMY);
-        alert(CONFIG.CARD_JUDGE_WIN("あいて"));
-        UI.addLog(CONFIG.CARD_JUDGE_WIN("あいて"));
+        alert(CONFIG.CARD_JUDGE_WIN(UI.ENEMY_NAME));
+        UI.addLog(CONFIG.CARD_JUDGE_WIN(UI.ENEMY_NAME));
     } else {
         alert(CONFIG.CARD_JUDGE_DROW);
         UI.addLog(CONFIG.CARD_JUDGE_DROW);
     }
+    if (minePoint === CONFIG.WIN_POINT || enemyPoint === CONFIG.WIN_POINT) {
+        endGame();
+        return;
+    }
 
+    // 全ターン終了でゲーム終了
+    if (turn === CONFIG.MAX_TURN) {
+        endGame();
+        return;
+    }
     // ターン表示更新
-    UI.updateTurn(++turn);
-    // 次ターンへ…
+    nextTurn();
+
 }
 
+// ターン表示更新
+export function nextTurn() {
+    turn++;
+    UI.updateTurn(turn);
+}
+
+// ゲーム終了時の判定
 export function endGame() {
-    
+
     UI.addLog(CONFIG.GAME_END);
 
     if (minePoint > enemyPoint) {
-        // alert(CONFIG.BATTLE_JUDGE_WIN("あなた"));
-        UI.addLog(CONFIG.BATTLE_JUDGE_WIN("あなた", minePoint, enemyPoint));
+        UI.addLog(CONFIG.BATTLE_JUDGE_WIN(UI.MINE_NAME, minePoint, enemyPoint));
     } else if (minePoint < enemyPoint) {
-        // alert(CONFIG.BATTLE_JUDGE_WIN("あいて"));
-        UI.addLog(CONFIG.BATTLE_JUDGE_WIN("あいて", minePoint, enemyPoint));
+        UI.addLog(CONFIG.BATTLE_JUDGE_WIN(UI.ENEMY_NAME, minePoint, enemyPoint));
     } else {
-        // alert(CONFIG.BATTLE_JUDGE_DROW);
-        UI.addLog(CONFIG.BATTLE_JUDGE_DROW("あなた", minePoint, enemyPoint));
+        UI.addLog(CONFIG.BATTLE_JUDGE_DROW(UI.MINE_NAME, minePoint, enemyPoint));
     }
+}
+
+export function processDecision() {
+
+    // ▼ 1. 自分のカードを描画
+    const mineIndex = UI.getSelectedMineIndex();
+    const mineCard = mineHand[mineIndex];
+    UI.renderOpenCard(mineCard, CONFIG.MINE);
+
+    UI.addLog(CONFIG.SELECT_CARD(UI.MINE_NAME, mineCard.name));
+
+    // ▼ 2. 相手の手札からランダムで選択
+    const enemyIndex = getRandomEnemyIndex();
+    const enemyCard = enemyHand[enemyIndex];
+
+    // ▼ 3. 相手カード描画
+    UI.renderOpenCard(enemyCard, CONFIG.ENEMY);
+    UI.addLog(CONFIG.SELECT_CARD(UI.ENEMY_NAME, enemyCard.name));
+
+    // ▼ 4. （必要なら）勝敗判定へ
+    cardJudge(mineCard, enemyCard, mineIndex, enemyIndex);
+}
+
+function getRandomEnemyIndex() {
+    const aliveIndexes = enemyHand
+        .map((card, index) => card ? index : null)
+        .filter(index => index !== null);
+
+    // aliveIndexes が [0, 2, 4] のようになる
+    const rand = Math.floor(Math.random() * aliveIndexes.length);
+    return aliveIndexes[rand];
 }

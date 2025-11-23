ff9ca3b67cc3395c35e318da91d44b908503c023
ringowomuite <huiqiteng05@gmail.com>
2025-11-23 15:25:27 +0900

    Update main.js
    確認メッセージを定数に変更
    クリックイベント後の内容をgame.jsに移動
    ログの出力内容の順序を変更

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ import * as CONFIG from "./config.js";
 import * as GAME from "./game.js";
 import * as UI from "./ui.js";
 
-let text;
-
 document.addEventListener("DOMContentLoaded", async () => {
     UI.addLog(CONFIG.INIT_START);
 
@@ -12,48 +10,18 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // オープンカード選択ボタンイベント作成
     document.getElementById("cardDecision").addEventListener("click", () => {
-        if (confirm("これで決定しますか？")) {
-            // ▼ 1. 自分のカードを描画
-            const mineIndex = UI.getSelectedMineIndex();
-            const mineCard = GAME.mineHand[mineIndex];
-            UI.renderOpenCard(mineCard, CONFIG.MINE);
-
-            text = "あなたは" + mineCard.name + "を選択しました。";
-            UI.addLog(text);
-
-            // ▼ 2. 相手の手札からランダムで選択
-            const enemyIndex = getRandomEnemyIndex();
-            const enemyCard = GAME.enemyHand[enemyIndex];
-
-            // ▼ 3. 相手カード描画
-            UI.renderOpenCard(enemyCard, CONFIG.ENEMY);
-            text = "あいては" + enemyCard.name + "を選択しました。";
-            UI.addLog(text);
-
-            // ▼ 4. （必要なら）勝敗判定へ
-            GAME.cardJudge(mineCard, enemyCard, mineIndex, enemyIndex);
-            
+        if (confirm(CONFIG.CONFIRM_DECISION)) {
+            GAME.processDecision();
         }
     });
 
     // ここに初期表示・初期設定などを書く
     // await も使える
 
-
+    UI.addLog(CONFIG.INIT_END);
 
     // ゲーム開始
     GAME.setupGame();
 
-    UI.addLog(CONFIG.INIT_END);
     UI.addLog(CONFIG.TURN_DISP(GAME.turn));
 });
-
-function getRandomEnemyIndex() {
-    const aliveIndexes = GAME.enemyHand
-        .map((card, index) => card ? index : null)
-        .filter(index => index !== null);
-
-    // aliveIndexes が [0, 2, 4] のようになる
-    const rand = Math.floor(Math.random() * aliveIndexes.length);
-    return aliveIndexes[rand];
-}

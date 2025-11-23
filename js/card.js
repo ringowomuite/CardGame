f28fc865bed0cad13bcda38403dc907e59c0d7fc
ringowomuite <huiqiteng05@gmail.com>
2025-11-23 17:45:24 +0900

    Update card.js
    カードにスキル効果を追加

--- js/card.js
+++ js/card.js
@@ -1,9 +1,54 @@
 export const cards = [
-  { id: 1, name: "シングル", base: 1, type: "none" },
-  { id: 2, name: "ダブル", base: 2, type: "none" },
-  { id: 3, name: "トリプル", base: 3, type: "none" },
-  { id: 4, name: "クアドラプル", base: 4, type: "none" },
-  { id: 5, name: "クインタプル", base: 5, type: "none" },
+  {
+    id: 1, name: "シングル", base: 1, type: "none",
+    skill: {
+      target_player: "mine",
+      target_card: "openCard",
+      type: "buff",
+      variable: "base",
+      plus: 1
+    }
+  },
+  {
+    id: 2, name: "ダブル", base: 2, type: "none",
+    skill: {
+      target_player: "mine",
+      target_card: "openCard",
+      type: "buff",
+      variable: "base",
+      plus: 1
+    }
+  },
+  {
+    id: 3, name: "トリプル", base: 3, type: "none",
+    skill: {
+      target_player: "mine",
+      target_card: "openCard",
+      type: "buff",
+      variable: "base",
+      plus: 1
+    }
+  },
+  {
+    id: 4, name: "クアドラプル", base: 4, type: "none",
+    skill: {
+      target_player: "mine",
+      target_card: "openCard",
+      type: "buff",
+      variable: "base",
+      plus: 1
+    }
+  },
+  {
+    id: 5, name: "クインタプル", base: 5, type: "none",
+    skill: {
+      target_player: "mine",
+      target_card: "openCard",
+      type: "buff",
+      variable: "base",
+      plus: 1
+    }
+  },
 ];
 
 export const cards2 = [
